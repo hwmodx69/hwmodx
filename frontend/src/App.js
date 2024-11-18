d66cd15910efe49c4d5edad3f8589338abce6684
hwmodx69 <hwmodx69@gmail.com>
2024-11-18 20:07:50 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,13 @@
+import React from "react";
+import Login from "./components/Login";
 
+function App() {
+  return (
+    <div>
+      <h1>E-commerce Site</h1>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
